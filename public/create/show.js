f78a68204d06918f424aacfccd27e671c7d11391
Andrew Urom <andrewurom@gmail.com>
2019-09-20 04:06:45 +0100

    Added tables

--- public/create/show.js
+++ public/create/show.js
@@ -4,15 +4,37 @@ $(document).ready(function(){
     
     function show(){
         let pinArray = [];
+        var now = new Date();
+
+        var day = ("0" + now.getDate()).slice(-2);
+
+        var month = ("0" + (now.getMonth() + 1)).slice(-2);
+
+        var today = now.getFullYear()+"-"+(month)+"-"+(day) ;
+
         $.get('http://localhost:3000/pin',function(data){
             $.each(data,function(i,pin){
                 pinArray.push(pin);
             })
-            
+            $('.row').append(`<table class="table table-dark">
+            <thead>
+              <tr>
+                <th scope="col">Id</th>
+                <th scope="col">Pin</th>
+                <th scope="col"></th>
+                <th scope="col"></th>
+                <th scope="col">Amount</th>
+                <th scope="col">Validity</th>
+              </tr>
+            </thead>
+            </table>`)
             for(var i=0; i<pinArray.length; i++){
                 let value = pinArray[i]["pin"];
                 let amount = pinArray[i]["amount"];
-                $('.row').append(`<div class="col-md-4"><li><a href="#">${value}</a></li><li>amount: ${amount}</li><li>Validity: Valid</li></div>`).css({
+                let id =pinArray[i]["id"];
+                //$('.row').append(`<div class="col-md-4"><li><a href="#">${value}</a></li><li>amount: ${amount}</li><li>Validity: Valid</li></div>`)
+                $('.row').append(`<table class="table table-dark">
+                <tbody><tr><th scope="row">${id}</th><td><a href="#"> ${value}</a></td><td>N${amount}</td><td>${today}</td></tr></tbody></table>`).css({
                     'background-color':'#ccc',
                     'list-style':'none',
                     'color':'#000',
